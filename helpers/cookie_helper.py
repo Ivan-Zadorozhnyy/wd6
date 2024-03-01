2d721ad75a0d8a051f53d7d7649d4fae8635be60
Ivan-Zadorozhnyy <119039373+Ivan-Zadorozhnyy@users.noreply.github.com>
2024-03-01 04:03:30 +0200

    Delete helpers directory
    fixes for hw7

--- helpers/cookie_helper.py
+++
@@ -1,5 +0,0 @@
-def set_cookie(response, key, value, http_only=True):
-    response.set_cookie(key, value, httponly=http_only)
-
-def get_cookie(request, name):
-    return request.COOKIES.get(name, 'Cookie not found')
--- helpers/header_helper.py
+++
@@ -1,5 +0,0 @@
-def set_header(response, name, value):
-    response[name] = value
-
-def get_header(request, name):
-    return request.headers.get(name, 'Header not found')
